e1769ace49ded47e68d7b1e27e823e0244f98fab
gitittttf <simon.erb.ddf@gmail.com>
2025-09-09 16:48:28 +0200

    test

--- src/room.java
+++ src/room.java
@@ -1,19 +1,19 @@
-
 import java.util.ArrayList;
 
 public class Room {
-
     ArrayList<Enemy> enemyList;
-
-    public Room(String[] enemiesInRoom) {
-        for (String enemy : enemiesInRoom) {
+    
+    // Constructor
+    public Room(EnemyType[] enemiesInRoom) {
+        this.enemyList = new ArrayList<Enemy>(0);
+        for (EnemyType enemy : enemiesInRoom) {
             enemyList = addEnemy(this.enemyList, enemy);
         }
     }
-
+    
+    //add Enemy to list
     ArrayList<Enemy> addEnemy(ArrayList<Enemy> enemyList, EnemyType enemy) {
         enemyList.add(new Enemy(enemy));
         return enemyList;
     }
-
 }
