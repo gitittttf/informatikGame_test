77a4a498a9c1671ced040be0b1989b7bbe6fcb50
gitittttf <simon.erb.ddf@gmail.com>
2025-09-09 16:18:49 +0200

    Added content to room.java

--- src/room.java
+++ src/room.java
@@ -1,8 +1,19 @@
 
+import java.util.ArrayList;
+
 public class room {
 
-    public room() {
+    ArrayList<Character> enemyList;
 
+    public room(String[] enemiesInRoom) {
+        for (String enemy : enemiesInRoom) {
+            enemyList = addEnemy(this.enemyList, enemy);
+        }
+    }
+
+    ArrayList<Character> addEnemy(ArrayList<Character> enemyList, String enemy) {
+        enemyList.add(new Character(EnemyType.MINI_ZOMBIE));
+        return enemyList;
     }
 
 }
