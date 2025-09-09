54772a7fe34c51e45861bdf7662a89a32c46e6bf
gitittttf <simon.erb.ddf@gmail.com>
2025-09-09 16:54:42 +0200

    Added comments

--- src/room.java
+++ src/room.java
@@ -1,19 +1,22 @@
+
 import java.util.ArrayList;
 
 public class Room {
+
     ArrayList<Enemy> enemyList;
-    
+
     // Constructor
+    /**
+     * Constructs a Room instance and populates it with enemies.
+     *
+     * @param enemiesInRoom An array of EnemyType representing the types of
+     * enemies to be added to the room. For each EnemyType in the array, a new
+     * Enemy instance is created and added to the room's enemy list.
+     */
     public Room(EnemyType[] enemiesInRoom) {
         this.enemyList = new ArrayList<Enemy>(0);
         for (EnemyType enemy : enemiesInRoom) {
-            enemyList = addEnemy(this.enemyList, enemy);
+            enemyList.add(new Enemy(enemy));
         }
-    }
-    
-    //add Enemy to list
-    ArrayList<Enemy> addEnemy(ArrayList<Enemy> enemyList, EnemyType enemy) {
-        enemyList.add(new Enemy(enemy));
-        return enemyList;
     }
 }
