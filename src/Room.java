a9763e943045f50df3221e84ba904fdff418e6f9
PurplestFox <ben.king7@outlook.com>
2025-09-09 16:48:02 +0200

    Add files via upload

--- src/Room.java
+++ src/Room.java
@@ -7,13 +7,7 @@ public class Room {
     public Room(EnemyType[] enemiesInRoom) {
         this.enemyList = new ArrayList<Enemy>(0);
         for (EnemyType enemy : enemiesInRoom) {
-            enemyList = addEnemy(this.enemyList, enemy);
+            enemyList.add(new Enemy(enemy));
         }
-    }
-    
-    //add Enemy to list
-    ArrayList<Enemy> addEnemy(ArrayList<Enemy> enemyList, EnemyType enemy) {
-        enemyList.add(new Enemy(enemy));
-        return enemyList;
     }
 }
