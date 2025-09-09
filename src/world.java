e94b1fa2140833f3e69cdadd816c3f96a012421e
gitittttf <simon.erb.ddf@gmail.com>
2025-09-09 12:43:25 +0200

    Added content to world.java

--- src/world.java
+++ src/world.java
@@ -1,4 +1,21 @@
+// World Klasse
+
+import java.util.ArrayList;
 
 public class world {
 
+    // Variabeln
+    int room_count;
+    ArrayList<room> roomList;
+
+    // Konstruktor
+    public world(int room_count, ArrayList<room> roomList) {
+        this.room_count = room_count;
+        this.roomList = roomList;
+    }
+
+    // Erzaehlt eine Story
+    public void erzaehle_story() {
+
+    }
 }
