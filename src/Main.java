3395da681edb5d7613f06cd04ea0cf0f8600a288
gitittttf <simon.erb.ddf@gmail.com>
2025-09-16 13:16:25 +0200

    Supress warning in main class

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        @SuppressWarnings("unused")
         World mainWorld = new World(new RoomType[]{RoomType.ZOMBIE_ROOM}); // 
         MainScreen.show();
     }
