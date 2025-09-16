94c6b4a57d3d8dda4cdffc4be5fb26f3b932e56d
paulnoahmaiss1-design <paulnoahmaiss1@gmail.com>
2025-09-16 12:57:41 +0200

    Update RoomType.java

--- src/main/java/RoomType.java
+++ src/main/java/RoomType.java
@@ -1,6 +1,16 @@
 public enum RoomType {
     //int lifeTotal, int armourValue, int initiative, int attack, int defense, int damage
     ZOMBIE_ROOM(new EnemyType[]{EnemyType.MINI_ZOMBIE, EnemyType.MINI_ZOMBIE});
+    INTRO_ROOM(new EnemyTpye[]{EnemyType.MINI_Zombie});
+    (new EnemyTpye[]{EnemyType.});
+    (new EnemyTpye[]{EnemyType.});
+    (new EnemyTpye[]{EnemyType.});
+    (new EnemyTpye[]{EnemyType.});
+    (new EnemyTpye[]{EnemyType.});
+    (new EnemyTpye[]{EnemyType.});
+    (new EnemyTpye[]{EnemyType.});    
+    (new EnemyTpye[]{EnemyType.});
+
 
     public EnemyType[] enemiesInRoom;
 
