cc87436f61c5dd88370ed0140a51607f2ab6a47b
gitittttf <simon.erb.ddf@gmail.com>
2025-09-16 13:09:04 +0200

    Little changes

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        World mainWorld = new World(new RoomType[]{RoomType.ZOMBIE_ROOM}); // 
         MainScreen.show();
-        FightManager fightmanagerroom1 = new FightManager(null, null);
     }
 }
