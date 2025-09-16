3921ace03cd13a21a2722dc53a9925bf9bedca6f
paulnoahmaiss1-design <paulnoahmaiss1@gmail.com>
2025-09-16 13:21:17 +0200

    Added content to RoomType.java

--- src/main/java/RoomType.java
+++ src/main/java/RoomType.java
@@ -1,15 +1,14 @@
 public enum RoomType {
     //int lifeTotal, int armourValue, int initiative, int attack, int defense, int damage
     ZOMBIE_ROOM(new EnemyType[]{EnemyType.MINI_ZOMBIE, EnemyType.MINI_ZOMBIE});
-    INTRO_ROOM(new EnemyTpye[]{EnemyType.MINI_Zombie});
-    (new EnemyTpye[]{EnemyType.});
-    (new EnemyTpye[]{EnemyType.});
-    (new EnemyTpye[]{EnemyType.});
-    (new EnemyTpye[]{EnemyType.});
-    (new EnemyTpye[]{EnemyType.});
-    (new EnemyTpye[]{EnemyType.});
-    (new EnemyTpye[]{EnemyType.});    
-    (new EnemyTpye[]{EnemyType.});
+    INTRO_ROOM(new EnemyType[]{EnemyType.MINI_Zombie});
+    FLOOR_ROOM(new EnemyType[]{EnemyType.MINI_ZOMBIE, EnemyType.MINI_ZOMBIE});
+    LIBRARY_ROOM(new EnemyType[]{EnemyType.MINI_ZOMBIE, EnemyType.MINI_ZOMBIE, EnemyType.SCIENTIST});
+    DININGHALL(new EnemyType[]{EnemyType.MINI_ZOMBIE, EnemyType.MINI_ZOMBIE, EnemyType.MINI_ZOMBIE, EnemyType.SCIENTIST, EnemyType.SCIENTIST});
+    LABORATORY(new EnemyType[]{EnemyType.SCIENTIST, EnemyType.SCIENTIST, EnemyType.SCIENTIST, EnemyType.SCIENTIST});
+    CORRIDOR(new EnemyType[]{EnemyType.BIG_ZOMBIE, EnemyType.BIG_ZOMBIE, EnemyType.BIG_ZOMBIE});
+    PANTRY(new EnemyTpye[]{EnemyType.MINI_ZOMBIE, EnemyType.MINI_ZOMBIE});
+    FINAL_ROOM(new EnemyType[]{EnemyType.ENDBOSS});
 
 
     public EnemyType[] enemiesInRoom;
