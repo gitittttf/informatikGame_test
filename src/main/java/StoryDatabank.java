8f58e9652ca16da5d19352ffbbacce7e5635ef46
paulnoahmaiss1-design <paulnoahmaiss1@gmail.com>
2025-09-23 12:13:47 +0200

    Update and rename Story.java to StoryDatabank.java

---
+++ src/main/java/StoryDatabank.java
@@ -0,0 +1,20 @@
+public enum StoryDatabank {
+
+    RAUM_1("Teststory");
+    RAUM_2("Teststory");
+    RAUM_3("Teststory");
+    RAUM_4("Teststory");
+    RAUM_5("Teststory");
+    RAUM_6("Teststory");
+    RAUM_7("Teststory");
+    RAUM_8("Teststory");
+    RAUM_9("Teststory");
+    RAUM_10("Teststory");
+
+
+    public String story;
+
+    StoryDatabank(String story) {
+        this.story = story;
+    }
+}
