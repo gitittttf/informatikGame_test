6cf15511dc4c2c6dbf1ca21e24dd2d708df5ac95
paulnoahmaiss1-design <paulnoahmaiss1@gmail.com>
2025-09-23 13:24:55 +0200

    Update UpgradeType.java

--- UpgradeType.java
+++ UpgradeType.java
@@ -1,6 +1,14 @@
 public enum UpgradeType {
     //int lifeTotal, int armourValue, int initiative, int attack, int defense, int damage, int finteLevel, int wuchtschlagLevel
-    FINTE_UPGRADE(0, 0, 0, 0, 0, 0, 1, 0);
+    FINTE_UPGRADE(0, 0, 0, 0, 0, 0, 1, 0),
+    LIFETOTAL_UPGRADE(3,0,0,0,0,0,0,0),
+    ARMOURVALUE_UPGRADE(0,2,0,0,0,0,0,0),
+    INITIATIVE_UPGRADE(0,0,3,0,0,0,0,0),
+    ATTACK_UPGRADE(0,0,0,3,0,0,0,0),
+    DEFENSE_UPGRADE(0,0,0,0,3,0,0,0),
+    DAMAGE_UPGRADE(0,0,0,0,0,3,0,0),
+    WUCHTSCHLAGLEVEL(0,0,0,0,0,0,0,2);
+    
 
     public int lifeTotal;
     public int armourValue;
